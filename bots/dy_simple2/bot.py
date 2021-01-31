d5ba01aa5c257bd7143b8e139cfd684b389dc2f1
dayoung100 <dayoung100@naver.com>
2021-01-31 23:40:55 +0900

    Update bot.py
    차원도약 대응 뺌

--- bots/dy_simple2/bot.py
+++ bots/dy_simple2/bot.py
@@ -1555,8 +1555,6 @@ class Bot(sc2.BotAI):
         if self.combat_strategy == 1 and self.tank_units.amount + self.units(UnitTypeId.VIKINGFIGHTER).amount >= 15 and self.combat_units.amount > 60:
             self.combat_strategy = 2
         #전체 후퇴 기준
-        if self.combat_strategy == 1 and self.known_enemy_units(UnitTypeId.BATTLECRUISER).exists and self.known_enemy_units(UnitTypeId.BATTLECRUISER).closest_to(self.cc).target_in_range(self.cc, 0.5) and self.fighting == 0:
-            self.combat_strategy == 0
         if self.combat_strategy >= 1 and (self.tank_units.amount + self.units(UnitTypeId.VIKINGFIGHTER).amount < 5 or self.marine_units.amount < 10):
             self.combat_strategy == 0
         if self.combat_strategy == 2 and (self.tank_units.amount + self.units(UnitTypeId.VIKINGFIGHTER).amount < 7 or self.marine_units.amount < 20):
